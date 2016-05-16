25ea113ae9f73f52bf7c2e542a7a8c9b47ded951
Tom Wayson <tom@tomwayson.com>
2016-05-16 12:13:54 -0700

    add all JSON files in charts folder instead of many app.import() calls
    Use a treeFor hook instead of mutilple app.import() calls for individual files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
+var BroccoliMergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-cli-cedar',
@@ -7,29 +10,24 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
+    // include bower dependencies
     app.import(app.bowerDirectory + '/d3/d3.js');
     app.import(app.bowerDirectory + '/vega/vega.js');
     app.import(app.bowerDirectory + '/arcgis-cedar/dist/cedar.js');
-    app.import(app.bowerDirectory + '/arcgis-cedar/dist/charts/bar-horizontal.json', {
-      destDir: 'assets/charts'
+  },
+
+  // include chart JSON files
+  treeForPublic: function(publicNode) {
+    var node = this._super.treeForPublic(publicNode);
+    var chartFiles = new Funnel(path.join(__dirname, 'bower_components', 'arcgis-cedar/dist/charts/'), {
+      include: ['**/*.json'],
+      destDir: '/assets/charts'
     });
-    app.import(app.bowerDirectory + '/arcgis-cedar/dist/charts/bar.json', {
-      destDir: 'assets/charts'
-    });
-    app.import(app.bowerDirectory + '/arcgis-cedar/dist/charts/bubble.json', {
-      destDir: 'assets/charts'
-    });
-    app.import(app.bowerDirectory + '/arcgis-cedar/dist/charts/pie.json', {
-      destDir: 'assets/charts'
-    });
-    app.import(app.bowerDirectory + '/arcgis-cedar/dist/charts/scatter.json', {
-      destDir: 'assets/charts'
-    });
-    app.import(app.bowerDirectory + '/arcgis-cedar/dist/charts/sparkline.json', {
-      destDir: 'assets/charts'
-    });
-    app.import(app.bowerDirectory + '/arcgis-cedar/dist/charts/time.json', {
-      destDir: 'assets/charts'
-    });
+
+    if (node) {
+      return new BroccoliMergeTrees([node, chartFiles]);
+    } else {
+      return chartFiles;
+    }
   }
 };
