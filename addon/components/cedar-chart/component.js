883accd6605cd78f0645689303f06d24857250cf
Markham Shofner <markham.shofner@gmail.com>
2016-08-04 18:26:11 -0400

    initial push towards cedar error message for invalid specs

--- addon/components/cedar-chart/component.js
+++ addon/components/cedar-chart/component.js
@@ -5,47 +5,46 @@ import layout from './template';
 export default Ember.Component.extend({
   layout: layout,
 
-  invalidSpecMessage: 'Invalid Cedar Specification',
+  invalidSpecMessage: 'Invalid Cedar Specification cli-cedar',
 
   // if in DOM and spec and options are valid
   // show chart at elememt
   _showChart() {
-    debugger;
     try {
-    if (this.isDestroyed || this.isDestroying) {
-      return;
+      if (this.isDestroyed || this.isDestroying) {
+        return;
+      }
+      const specification = this.get('specification');
+      if (!specification) {
+        return;
+      }
+      const options = this.get('options') || {};
+      const override = this.get('override') || options.override;
+
+      // use elementId from component to render Cedar directly into div
+      options.elementId = '#' + this.elementId;
+
+      // autolabels will throw an error on pie charts
+      // see https://github.com/Esri/cedar/issues/173
+      if (specification.type === 'pie') {
+        options.autolabels = false;
+      }
+
+      // create the chart
+      var chart = new Cedar(specification);
+
+      // attach the chart to the DOM
+      chart.show(options);
+
+      // look for overrides & apply
+      if (override) {
+        chart.override = override;
+      }
     }
-    const specification = this.get('specification');
-    if (!specification) {
-      return;
+    catch(err) {
+      this.$().text(this.get('invalidSpecMessage'));
     }
-    const options = this.get('options') || {};
-    const override = this.get('override') || options.override;
-
-    // use elementId from component to render Cedar directly into div
-    options.elementId = '#' + this.elementId;
-
-    // autolabels will throw an error on pie charts
-    // see https://github.com/Esri/cedar/issues/173
-    if (specification.type === 'pie') {
-      options.autolabels = false;
-    }
-
-    // create the chart
-    var chart = new Cedar(specification);
-
-    // attach the chart to the DOM
-    chart.show(options);
-
-    // look for overrides & apply
-    if (override) {
-      chart.override = override;
-    }
-  }
-  catch(err) {
-    this.$().text(this.get('invalidSpecMessage'));
-  }
-},
+  },
 
   // show/update chart whenever attributes change
   didReceiveAttrs() {
