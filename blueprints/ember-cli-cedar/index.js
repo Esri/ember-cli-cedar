80f03f45a45cc3d8ed6b644e3dc172233e70c103
Tom Wayson <tom@tomwayson.com>
2016-12-21 14:38:40 -0800

    revert back to cedar 0.6.1 b/c 0.7.0 breaks this addon

--- blueprints/ember-cli-cedar/index.js
+++ blueprints/ember-cli-cedar/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('arcgis-cedar', '^0.7.0');
+    return this.addBowerPackageToProject('arcgis-cedar', '^0.6.1');
   }
 };
