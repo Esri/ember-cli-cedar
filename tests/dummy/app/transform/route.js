95d72a7efaa13d5c02a2b7ef9bd387a09224f012
Tom Wayson <tom@tomwayson.com>
2017-10-16 16:35:23 -0700

    fix, but hide transform route in dummy app
    got transform working, but hiding the route in the dummy app
    b/c that example doesn't make much sense when using AmCharts

--- tests/dummy/app/transform/route.js
+++ tests/dummy/app/transform/route.js
@@ -1,16 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model () {
+  model() {
     return {
       type: 'pie',
-      options: {
-        width: 600,
-        height: 600,
-        autolabels: false
-      },
-      dataset: {
-        "url":"https://services.arcgis.com/uDTUpUPbk8X8mXwl/arcgis/rest/services/Public_Schools_in_Onondaga_County/FeatureServer/0",
+      datasets: [{
+        "name": "students",
+        "url": "https://services.arcgis.com/uDTUpUPbk8X8mXwl/arcgis/rest/services/Public_Schools_in_Onondaga_County/FeatureServer/0",
         "query": {
           "orderByFields": "Number_of_SUM DESC",
           "groupByFieldsForStatistics": "Type",
@@ -19,12 +15,25 @@ export default Ember.Route.extend({
             "onStatisticField": "Number_of",
             "outStatisticFieldName": "Number_of_SUM"
           }]
-        },
-        "mappings":{
-          "label": {"field":"Type","label":"Type"},
-          "y": {"field":"Number_of_SUM","label":"Total Students"},
-          "radius": 270
         }
+      }],
+      series: [{
+        "source": "students",
+        "category": {
+          "field": "Type",
+          "label": "Type"
+        },
+        "value": {
+          "field": "Number_of_SUM",
+          "label": "Total Students"
+        }
+      }],
+      // in amCharts, you don't need a transform to group small pie slices
+      // but for this contrived example, we'll override the default behavior
+      // so we can use the existing transform code
+      // TODO: use a better example
+      overrides: {
+        groupPercent: 0
       }
     };
   }
