501fe22589197eefe47050ce3c60007522989220
Ben Stoltz <bstoltz@esri.com>
2017-10-17 15:10:20 -0400

    Fix lint error

--- tests/dummy/app/playground/route.js
+++ tests/dummy/app/playground/route.js
@@ -4,25 +4,25 @@ export default Ember.Route.extend({
   model: function() {
     // bubble scatterplot JSON
     return {
-  "type": "scatter",
-  "datasets": [
-    {
-      "url": "https://services1.arcgis.com/bqfNVPUK3HOnCFmA/arcgis/rest/services/Demographics_(Median_Household_Income)/FeatureServer/0",
-      "append": true
-    }
-  ],
-  "series": [
-    {
-      "category": {
-        "field": "TotalPop2015",
-        "label": "Population"
-      },
-      "value": {
-        "field": "MedianHHIncome2015",
-        "label": "Median Median Household Income"
-      }
-    }
-  ]
-}
+      "type": "scatter",
+      "datasets": [
+        {
+          "url": "https://services1.arcgis.com/bqfNVPUK3HOnCFmA/arcgis/rest/services/Demographics_(Median_Household_Income)/FeatureServer/0",
+          "append": true
+        }
+      ],
+      "series": [
+        {
+          "category": {
+            "field": "TotalPop2015",
+            "label": "Population"
+          },
+          "value": {
+            "field": "MedianHHIncome2015",
+            "label": "Median Median Household Income"
+          }
+        }
+      ]
+    };
   },
 });
