0a297349200ebe512ceef5be0d387360743d7116
Tom Wayson <tom@tomwayson.com>
2017-10-18 12:28:09 -0700

    prep for v1-alpha release

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,6 @@ module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     // Add options here
     cedar: {
-      // include amCharts scripts/styles in vendor files
       amCharts: {
         // publicPath: amCharts will be included at this path in the public folder
         // use this if you are not loading amCharts from the CDN
@@ -18,13 +17,6 @@ module.exports = function(defaults) {
       }
     }
   });
-
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
 
   return app.toTree();
 };
