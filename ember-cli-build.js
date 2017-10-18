a266f4c55d3f70a7e71a510f32f82a0cb05b68ae
Tom Wayson <tom@tomwayson.com>
2017-10-18 15:59:41 -0700

    don't use export plugin b/c gh-pages won't serve the js assets

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,7 +11,7 @@ module.exports = function(defaults) {
         // use this if you are not loading amCharts from the CDN
         publicPath: 'amcharts',
         // imports: an array of amcharts files to be imported (bundled) in vendor files
-        imports: ['amcharts.js', 'serial.js', 'xy.js', 'pie.js', 'radar.js', 'plugins/export/export.js', 'plugins/export/export.css']
+        imports: ['amcharts.js', 'serial.js', 'xy.js', 'pie.js', 'radar.js']
         // TODO: srcDir: '/path/to/local/amCharts'
         // otherwise defaults to installing from node_modules
       }
