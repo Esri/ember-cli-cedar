4b172d36f49c97485b630f1f19789d0537592d66
Tom Wayson <tom@tomwayson.com>
2017-10-18 14:53:08 -0700

    AmCharts path should be relative to rootURL
    dummy app fetches chart .json relative to rootURL

--- tests/dummy/app/charts/chart/route.js
+++ tests/dummy/app/charts/chart/route.js
@@ -1,12 +1,13 @@
 import fetch from 'fetch';
 import Ember from 'ember';
+import ENV from '../../config/environment';
 
 export default Ember.Route.extend({
   model: function(params) {
     // TODO: default to 'bar'
     const slug = params.slug;
     // fetch the example chart definition JSON
-    return fetch (`/examples/${slug}.json`)
+    return fetch (`${ENV.rootURL}examples/${slug}.json`)
     .then(response => {
       // TODO: check if response is OK
       return response.json()
