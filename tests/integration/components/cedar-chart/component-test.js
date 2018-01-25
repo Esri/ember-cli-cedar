ac70a069b16c942aaa0c024fdbbe7529f92af5ad
Ben Stoltz <bstoltz@esri.com>
2018-01-25 07:59:05 -0500

    Im a dunce on tests...

--- tests/integration/components/cedar-chart/component-test.js
+++ tests/integration/components/cedar-chart/component-test.js
@@ -59,3 +59,40 @@ test('It generates a chart properly', function (assert) {
   });
   this.render(hbs`{{cedar-chart definition=definition onUpdateEnd=(action 'updateEnd')}}`);
 });
+
+test('It properly timesout', function (assert) {
+  let done = assert.async();
+  this.on('onError', function (err) {
+    assert.equal(err, 'timeout', 'chart was rendered');
+    done();
+  });
+  this.set('definition', {
+    'type': 'bar',
+    'datasets': [
+      {
+        'url': 'https://services.arcgis.com/uDTUpUPbk8X8mXwl/arcgis/rest/services/Public_Schools_in_Onondaga_County/FeatureServer/0',
+        'name': 'Number_of_SUM',
+        'query': {
+          'orderByFields': 'Number_of_SUM DESC',
+          'groupByFieldsForStatistics': 'Type',
+          'outStatistics': [
+            {
+              'statisticType': 'sum',
+              'onStatisticField': 'Number_of',
+              'outStatisticFieldName': 'Number_of_SUM'
+            }
+          ]
+        }
+      }
+    ],
+    'series': [
+      {
+        'category': {'field': 'Type', 'label': 'Type'},
+        'value': {'field': 'Number_of_SUM', 'label': 'Number of Students'},
+        'source': 'Number_of_SUM'
+      }
+    ]
+  });
+  this.set('timeout', 1);
+  this.render(hbs`{{cedar-chart definition=definition timeout=timeout onError=(action 'onError')}}`);
+});
