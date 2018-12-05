b477e8efeff2889a9c5102cb20fb098db5086655
john gravois <jgravois@esri.com>
2018-12-05 10:38:19 -0800

    keep using loading chart json from surge

--- tests/dummy/app/charts/chart/route.js
+++ tests/dummy/app/charts/chart/route.js
@@ -7,19 +7,19 @@ export default Route.extend({
     // TODO: default to 'bar'
     const slug = params.slug;
     // fetch the example chart definition JSON from the cedar v1 docs site
-    return fetch (`https://raw.githubusercontent.com/Esri/cedar/master/docs/examples/${slug}.json`)
-    .then(response => {
-      // TODO: check if response is OK
-      return response.json()
-      .then(definition => {
-        const title = `${capitalize(params.slug)} Example`;
-        definition.legend = { visible: true};
-        return {
-          title,
-          definition,
-          slug
-        };
+    return fetch (`https://cedar-v1.surge.sh/examples/${slug}.json`)
+      .then(response => {
+        // TODO: check if response is OK
+        return response.json()
+        .then(definition => {
+          const title = `${capitalize(params.slug)} Example`;
+          definition.legend = { visible: true};
+          return {
+            title,
+            definition,
+            slug
+          };
+        });
       });
-    });
   }
 });
